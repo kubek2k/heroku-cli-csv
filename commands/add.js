8c87a8d4c4865127f414ec3d71393469a81a2549
Jakub Janczak <jakub.janczak@schibsted.pl>
2015-12-14 20:59:21 +0100

    Making an exception for args names variables because they are displayed better in help

--- commands/add.js
+++ commands/add.js
@@ -3,12 +3,12 @@ const cli = require('heroku-cli-util');
 const csv = require('./heroku-csv.js');
 
 function addCommand(context, heroku) {
-  const key = context.args.configKey;
+  const key = context.args.config_key;
   const app = heroku.apps(context.app);
 
   return csv.getConfigArray(app, key)
     .then((originalArr) => {
-      const valueToAdd = context.args.valueToAdd;
+      const valueToAdd = context.args.value_to_add;
       if (originalArr.indexOf(valueToAdd) >= 0) {
         cli.warn(`Config variable ${cli.color.cyan(key)} already contains ${valueToAdd}`);
       } else {
@@ -25,7 +25,7 @@ module.exports = {
   topic: 'csv',
   command: 'add',
   description: 'Adds value to CSV config variable, if its not already there',
-  args: [{name: 'configKey'}, {name: 'valueToAdd'}],
+  args: [{name: 'config_key'}, {name: 'value_to_add'}],
   needsApp: true,
   needsAuth: true,
   run: cli.command(addCommand)
